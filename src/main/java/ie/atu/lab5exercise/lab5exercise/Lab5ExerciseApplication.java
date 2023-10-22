c2e05295e480895815ccbdc09cfaf990be7ed53b
conorkeane <g00391846@atu.ie>
2023-10-22 20:03:42 +0100

    Created a new project for Exercise 2

---
+++ src/main/java/ie/atu/lab5exercise/lab5exercise/Lab5ExerciseApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab5exercise.lab5exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5ExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5ExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab5exercise/lab5exercise/Lab5ExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab5exercise.lab5exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5ExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
